30d6be6ae2019baa35d1ea4bc0ca52072b82af33
erzhu4@gmail.com <erzhu4@gmail.com>
2018-06-19 21:50:29 -0400

    add twilio

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Twilio\Rest\Client;
 
 class TestController extends Controller
 {
@@ -22,6 +23,24 @@ class TestController extends Controller
      */
     public function index()
     {
-        return view("test/test");
+        // Your Account SID and Auth Token from twilio.com/console
+        // $account_sid = 'AC4f37b45fe0507f9b0e7b2762826a5436';
+        // $auth_token = 'a3f197359d6cc964639e838fbeda62c8';
+        // In production, these should be environment variables. E.g.:
+        // $auth_token = $_ENV["TWILIO_ACCOUNT_SID"]
+
+        // A Twilio number you own with SMS capabilities
+        // $twilio_number = "+19704091390";
+
+        // $client = new Client($account_sid, $auth_token);
+        // $client->messages->create(
+        //     '+12023048498',
+        //     array(
+        //         'from' => $twilio_number,
+        //         'body' => 'EGGY!!!'
+        //     )
+        // );
+
+        return "tested";
     }
 }
